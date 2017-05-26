7a45252f98491e3bacdf96b8fd1584df4b8c6031
Aiman Yusra <aimanyusra@hotmail.com>
2017-05-26 15:55:33 +0800

    migrated new users column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,12 @@
-class Clearance::UsersController < Clearance::BaseController
-  if respond_to?(:before_action)
-    before_action :redirect_signed_in_users, only: [:create, :new]
-    skip_before_action :require_login, only: [:create, :new], raise: false
-    skip_before_action :authorize, only: [:create, :new], raise: false
-  else
-    before_filter :redirect_signed_in_users, only: [:create, :new]
-    skip_before_filter :require_login, only: [:create, :new], raise: false
-    skip_before_filter :authorize, only: [:create, :new], raise: false
-  end
+class UsersController < Clearance::UsersController
 
   def new
-    @user = user_from_params
+    @user = User.new()
     render template: "users/new"
   end
 
   def create
-    @user = user_from_params
+    @user = User.new(user_params)
 
     if @user.save
       sign_in @user
@@ -45,17 +36,22 @@ class Clearance::UsersController < Clearance::BaseController
     Clearance.configuration.redirect_url
   end
 
-  def user_from_params
-    email = user_params.delete(:email)
-    password = user_params.delete(:password)
+  # def user_from_params
+  #   email = user_params.delete(:email)
+  #   password = user_params.delete(:password)
 
-    Clearance.configuration.user_model.new(user_params).tap do |user|
-      user.email = email
-      user.password = password
-    end
-  end
+  #   Clearance.configuration.user_model.new(user_params).tap do |user|
+  #     user.email = email
+  #     user.password = password
+  #   end
+  # end
 
   def user_params
-    params[Clearance.configuration.user_parameter] || Hash.new
+  	params.require(:user).permit(:first_name, :last_name, :age, :email, :password)
   end
+  # def user_params
+  #   params[Clearance.configuration.user_parameter] || Hash.new
+  # end
 end
+
+
