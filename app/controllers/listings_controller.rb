2827815a77f65514b1ab905622fbe460936e738b
Aiman Yusra <aimanyusra@hotmail.com>
2017-05-31 11:27:06 +0800

    added verified to listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,9 +2,11 @@ class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
 
-def verify
-byebug
-end
+  def verify
+    @listings = Listing.find(params[:listing_id])
+    @listings.verified!
+    redirect_to listings_path
+  end
   # GET /listings
   # GET /listings.json
   def index
