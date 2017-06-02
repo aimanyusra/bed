dade29d998280bc3dcfbdb9eb5ff8922a9fa6a2e
Aiman Yusra <aimanyusra@hotmail.com>
2017-06-02 13:17:17 +0800

    added date overlap validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :listing
 
+  validate :check_overlapping_dates
+  validate :check_max_guests
+
+  def check_overlapping_dates
+  	errors.add(:overlapping_dates, "The booking dates conflict with existing bookings")
+  end
+
+  def check_max_guests
+
+  end
+
   
 end
