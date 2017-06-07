c12a54e0a2088a1ed307e829f13a10c27942b7d8
Aiman Yusra <aimanyusra@hotmail.com>
2017-06-07 17:30:50 +0800

    fix ajax validation js

--- app/assets/javascripts/ajax_validation.js
+++ app/assets/javascripts/ajax_validation.js
@@ -2,34 +2,38 @@ $.ajaxSetup({
   dataType: 'json'
 })
 
-$("#new_user").on("ajax:success", (e, data, status, xhr) ->
+$("#new_user").on("ajax:success", function(e, data, status, xhr){
   $("#users").append("<li>" + data['name'] + "</li>")
   $("#users").effect("highlight")
-).on("ajax:error", (e, data, status, xhr) ->
+}).on("ajax:error", function(e, data, status, xhr){
   $("#step-users form").render_form_errors('user', data.responseJSON)
-)
+})
 
-$.fn.render_form_errors = (model_name, errors) ->
+$.fn.render_form_errors = function(model_name, errors){
   form = this
   this.clear_form_errors()
 
-  $.each(errors, (field, messages) ->
-    input = form.find('input, select, textarea').filter(->
+  $.each(errors, function(field, messages){
+    input = form.find('input, select, textarea').filter(function(){
       name = $(this).attr('name')
-      if name
+      if(name) {
         name.match(new RegExp(model_name + '\\[' + field + '\\(?'))
-    )
+      }
+    })
     input.closest('.form-group').addClass('has-error')
-    input.parent().append('<span class="help-block">' + $.map(messages, (m) -> m.charAt(0).toUpperCase() + m.slice(1)).join('<br />') + '</span>')
-  )
+    input.parent().append('<span class="help-block">' + $.map(messages, function(m){ return (m.charAt(0).toUpperCase() + m.slice(1)).join('<br />') + '</span>'} ))
+  })
 
-  $.fn.clear_form_errors = () ->
-  this.find('.form-group').removeClass('has-error')
-  this.find('span.help-block').remove()
+  $.fn.clear_form_errors = function(){
+    this.find('.form-group').removeClass('has-error')
+    this.find('span.help-block').remove()
+  }
+}
 
-$.fn.clear_form_fields = () ->
+$.fn.clear_form_fields = function(){
   this.find(':input','#myform')
       .not(':button, :submit, :reset, :hidden')
       .val('')
       .removeAttr('checked')
       .removeAttr('selected')
+}
