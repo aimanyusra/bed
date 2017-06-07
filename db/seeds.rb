05f17b6dfe642622bccf10fea29d6abd3aaa5bef
Aiman Yusra <aimanyusra@hotmail.com>
2017-06-07 17:56:09 +0800

    config seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,22 +31,22 @@ User.all.each { |u| uids << u.id }
 
 ActiveRecord::Base.transaction do
   40.times do 
-    # listing['name'] = Faker::App.name
+    listing['name'] = Faker::App.name
     # listing['place_type'] = rand(1..3)
     # listing['property_type'] = ["House", "Entire Floor", "Condominium", "Villa", "Townhouse", "Castle", "Treehouse", "Igloo", "Yurt", "Cave", "Chalet", "Hut", "Tent", "Other"].sample
 
     # listing['room_number'] = rand(0..5)
-    # listing['num_of_bedrooms'] = rand(1..6)
+    listing['num_of_bedrooms'] = rand(1..6)
     listing['max_guests'] = rand(1..10)
 
     # listing['country'] = Faker::Address.country
     # listing['state'] = Faker::Address.state
     # listing['city'] = Faker::Address.city
     # listing['zipcode'] = Faker::Address.zip_code
-    # listing['address'] = Faker::Address.street_address
+    listing['address'] = Faker::Address.street_address
 
-    # listing['price'] = rand(80..500)
-    # listing['description'] = Faker::Hipster.sentence
+    listing['price'] = rand(80..500)
+    listing['description'] = Faker::Hipster.sentence
 
     listing['user_id'] = uids.sample
 
